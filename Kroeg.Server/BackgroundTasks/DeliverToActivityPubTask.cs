081d3de2d65fdc63d81544aee8d447d44b1b98c6
Puck Meerburg <puck@puckipedia.nl>
2017-06-17 00:13:31 +0200

    Server: Unflatten object before delivering it

--- Kroeg.Server/BackgroundTasks/DeliverToActivityPubTask.cs
+++ Kroeg.Server/BackgroundTasks/DeliverToActivityPubTask.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Kroeg.Server.Models;
 using Kroeg.Server.Services.EntityStore;
+using Kroeg.Server.Tools;
+using Newtonsoft.Json;
 
 namespace Kroeg.Server.BackgroundTasks
 {
@@ -17,18 +19,21 @@ namespace Kroeg.Server.BackgroundTasks
     public class DeliverToActivityPubTask : BaseTask<DeliverToActivityPubData, DeliverToActivityPubTask>
     {
         private readonly IEntityStore _entityStore;
+        private readonly EntityFlattener _entityFlattener;
 
-        public DeliverToActivityPubTask(EventQueueItem item, IEntityStore entityStore) : base(item)
+        public DeliverToActivityPubTask(EventQueueItem item, IEntityStore entityStore, EntityFlattener entityFlattener) : base(item)
         {
             _entityStore = entityStore;
+            _entityFlattener = entityFlattener;
         }
 
         public override async Task Go()
         {
             var entity = await _entityStore.GetEntity(Data.ObjectId, false);
+            var unflattened = await _entityFlattener.Unflatten(_entityStore, entity);
 
             var hc = new HttpClient();
-            var serialized = entity.SerializedData;
+            var serialized = unflattened.Serialize(true).ToString(Formatting.None);
             var content = new StringContent(serialized, Encoding.UTF8);
             content.Headers.ContentType = new MediaTypeHeaderValue("application/ld+json");
             content.Headers.ContentType.Parameters.Add(new NameValueHeaderValue("profile", "\"https://www.w3.org/ns/activitystreams\""));
